c6ce945935f5be389a8c4deac785cbe0ef2ad2c4
Ling Igor <lingigor1990@gmail.com>
2023-02-21 18:48:51 +0200

    task 6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", outBlur);
 
 function outBlur(event) {
-  if (event.currentTarget.value.length >= Number(input.dataset.length)) {
+  if (event.currentTarget.value.length === Number(input.dataset.length)) {
     input.classList.add("valid");
     input.classList.remove("invalid");
   } else {
